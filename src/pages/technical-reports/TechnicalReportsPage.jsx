c3bb88378abddfb4b97c80ee543e0926b6d02aac
Daniel Davies <85223934+DanielJamesDavies@users.noreply.github.com>
2024-11-16 18:32:41 +0000

    Technical Reports Page

--- src/pages/technical-reports/TechnicalReportsPage.jsx
+++ src/pages/technical-reports/TechnicalReportsPage.jsx
@@ -18,6 +18,27 @@ export const TechnicalReportsPage = () => {
 		<div className='page technical-reports-page'>
 			<div className='page-content'>
 				<div className='page-title'>Technical Reports</div>
+				<div className='technical-reports-list'>
+					{[
+						{ title: "Discovering Latent Connections", image: "images/Latent Connections/preview.png" },
+						{ title: "Using TopK Sparse Autoencoders on Turing-LLM-1.0-254M", image: "images/Sparse Autoencoders/preview.png" },
+						{ title: "Building Turing-LLM-1.0-254M", image: "images/Turing-LLM-1.0-254M/preview.png" },
+					]?.map((technicalReportItem, index) => (
+						<div key={index} className='technical-reports-list-item'>
+							<div className='technical-reports-list-item-image'>
+								<img src={technicalReportItem?.image || "/images/preview.png"} />
+							</div>
+							<div className='technical-reports-list-item-title'>
+								{technicalReportItem?.title?.split("-")?.map((str, i) => (
+									<>
+										<span>{str}</span>
+										{i + 1 >= technicalReportItem?.title?.split("-")?.length ? null : <span>&#8209;</span>}
+									</>
+								))}
+							</div>
+						</div>
+					))}
+				</div>
 			</div>
 		</div>
 	);
