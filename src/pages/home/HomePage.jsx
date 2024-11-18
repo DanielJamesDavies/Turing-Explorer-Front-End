6bc3dbd859970be2583064c2f884b3f581df12d2
Daniel Davies <85223934+DanielJamesDavies@users.noreply.github.com>
2024-11-18 01:57:09 +0000

    Improved Home Page

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -22,10 +22,52 @@ export const HomePage = () => {
 		<div className='page home-page'>
 			<div className='home-hero'>
 				<div className='home-title'>Turing Explorer</div>
-				<div className='home-hero-text'>To understand the internals of a large language model.</div>
+				<div className='home-hero-text'>
+					A mechanistic interpretability tool to understand the internals of Turing-LLM, a large language model.
+				</div>
 				<div className='home-primary-buttons'>
 					<button onClick={() => navigate("/download")}>Download Turing-LLM Explorer</button>
 					<button onClick={() => navigate("/technical-reports")}>Read Technical Reports</button>
+				</div>
+			</div>
+			<div className='home-section'>
+				<div className='home-text'>
+					Welcome to Turing Explorer, a cutting-edge research tool designed to advance the study and understanding of the inner mechanisms
+					of large language models. At its core is Turing-LLM, a novel large language model crafted to simplify and enhance humanity's
+					comprehension of how these models operate. Join us in exploring the future of AI and unlock the secrets behind language models
+					with Turing Explorer.
+				</div>
+			</div>
+			<div className='home-section'>
+				<div className='home-subtitle'>The Mission</div>
+				<div className='home-text'>
+					By deeply understanding how AI models operate, we have the potential to significantly enhance human intelligence. Through
+					understanding and integrating advanced algorithms found in deep learning models—either by learning them ourselves or by
+					enhancing our brains—we can adopt more effective problem-solving strategies. A comprehensive mechanistic understanding of
+					superintelligent systems can open up opportunities for a coevolutionary journey towards a safer and smarter future.
+				</div>
+			</div>
+			<div className='home-section'>
+				<div className='home-subtitle'>Progress</div>
+				<div className='home-progress-list'>
+					{[
+						{ label: "Generated a 2B Token Synthetic Dataset", status: "done" },
+						{ label: "Built and Trained Turing-LLM-1.0-254M", status: "done" },
+						{ label: "Trained Sparse Autoencoders on LLM Activations", status: "done" },
+						{ label: "Developing & Evaluating Novel Interpretability Approaches", status: "doing" },
+						{ label: "Assembling a Tool to Explore Turing-LLM", status: "doing" },
+					]?.map((progressItem, index) => (
+						<div key={index} className='home-progress-item'>
+							{progressItem?.status === "done" ? (
+								<i className='fa-solid fa-circle-check'></i>
+							) : progressItem?.status === "doing" ? (
+								<i className='fa-solid fa-circle-notch'></i>
+							) : (
+								<i className='fa-regular fa-circle'></i>
+							)}
+							<div className='home-progress-item-label'>{progressItem?.label}</div>
+						</div>
+					))}
 				</div>
 			</div>
 			<div className='home-section'>
@@ -52,29 +94,6 @@ export const HomePage = () => {
 					</ul>
 					<br />
 					Conclusion: This may be the best path to true ensured safety, not only through aligning models, but through coevolving together.
-				</div>
-			</div>
-			<div className='home-section'>
-				<div className='home-subtitle'>Progress</div>
-				<div className='home-progress-list'>
-					{[
-						{ label: "Generated a 2B Token Synthetic Dataset", status: "done" },
-						{ label: "Built and Trained Turing-LLM-1.0-254M", status: "done" },
-						{ label: "Trained Sparse Autoencoders on LLM Activations", status: "done" },
-						{ label: "Developing & Evaluating Novel Interpretability Approaches", status: "doing" },
-						{ label: "Assembling a Tool to Explore Turing-LLM", status: "doing" },
-					]?.map((progressItem, index) => (
-						<div key={index} className='home-progress-item'>
-							{progressItem?.status === "done" ? (
-								<i className='fa-solid fa-circle-check'></i>
-							) : progressItem?.status === "doing" ? (
-								<i className='fa-solid fa-circle-notch'></i>
-							) : (
-								<i className='fa-regular fa-circle'></i>
-							)}
-							<div className='home-progress-item-label'>{progressItem?.label}</div>
-						</div>
-					))}
 				</div>
 			</div>
 		</div>
